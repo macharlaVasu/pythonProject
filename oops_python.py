7c8f01db01f77acb461baa90ae622da430860c94
macharlaVasu <m.vasu14@gmail.com>
2025-01-31 16:46:15 +0530

    added the oops concepts file

---
+++ oops_python.py
@@ -0,0 +1,25 @@
+'''
+oops concepts example programs
+1. Class 
+2. Objects 
+3. Inheritance
+4. Polymorphism
+5. Encapsulation 
+6. Abstract 
+
+'''
+# 1 Class 
+
+class Animai:
+    def __init__(self,name):
+        self.name =name
+    def hellow(self):
+        print(f'Hellow {self.name}')    
+
+
+obj = Animai('Rubby')
+obj.hellow()
+
+
+obj1 = Animai('canddy')
+obj1.hellow()
