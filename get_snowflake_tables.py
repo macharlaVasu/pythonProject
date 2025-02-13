38416dda6648f03723a925ca20ecd7fcb3d91363
macharlaVasu <m.vasu14@gmail.com>
2025-02-13 13:55:00 +0530

    read snowflake table generate the csv file

---
+++ get_snowflake_tables.py
@@ -0,0 +1,22 @@
+# main.py
+from snowflake_connection import create_snowflake_connection
+import pandas as pd
+
+conn =create_snowflake_connection()
+
+print(conn)
+
+query = "select * from emp;"
+
+df = pd.read_sql_query(query,conn)
+
+conn.close()
+
+print (df.head())
+
+file_path ='/Users/sreenivasulumacharla/Documents/Test_files/snowflake_emp.csv'    
+
+
+df.to_csv(file_path,index=None)
+
+print(f"File Saved to: {file_path}")
